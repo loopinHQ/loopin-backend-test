6e537aa871d31f3e450ef65f602838513d101451
Alex Hughes <alex@alexjackhughes.com>
2022-07-27 10:27:02 +0100

    Add todo stub component

---
+++ components/AddTodo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddTodo: React.FC = () => {
+  return (
+    <div className="bg-red-200 w-full my-8">
+      <p>add todo</p>
+    </div>
+  );
+};
+
+export default AddTodo;
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Todos } from "../models/Todo";
+import AddTodo from "./AddTodo";
 import TodoItem from "./TodoItem";
 
 interface Props {
@@ -9,6 +10,7 @@ interface Props {
 const TodoList: React.FC<Props> = ({ todos = [] }) => {
   return (
     <div className="flex flex-col grow items-center mx-auto max-w-screen-xl overflow-y-scroll">
+      <AddTodo />
       {todos.map((todo) => (
         <TodoItem key={todo.id} todo={todo} />
       ))}
